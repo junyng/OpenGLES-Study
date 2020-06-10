bbd2c89762399b7410dc77bb5c662983ee15b4a1
junyng <pom0894@naver.com>
2020-06-10 21:36:35 +0900

    add initial project

---
+++ OpenGLES-Samples/OpenGLES-Samples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenGLES-Samples
+//
+//  Created by BLU on 2020/06/10.
+//  Copyright © 2020 BLU. All rights reserved.
+//
+
+import GLKit
+
+class ViewController: GLKViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
